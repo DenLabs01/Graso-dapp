0b57db614f8f562d2021399295d5fa6944cc3c7a
codeAKstan <boystanismoses@gmail.com>
2025-04-26 11:31:40 +0100

    Add Hero section only

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Hero from "@/components/hero/Hero";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <Navbar />
       <Hero />
       {/* Other sections will go here */}
     </main>
