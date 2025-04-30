14f958109087e6c5922886cd83d9d624e77c1a8e
Kalu Victor <victorbuikemdev@gmail.com>
2025-04-30 03:37:21 +0000

    update for this branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/navbar";
+import {Features} from "@/components/features";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Features />
       The Landing Page Here
     </main>
   )
